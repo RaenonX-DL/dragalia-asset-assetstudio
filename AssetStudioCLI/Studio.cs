297829cfd4f6fff3972abcd994df8c23100d27a5
RaenonX <raenonx0710@gmail.com>
2021-01-08 02:07:16 -0600

    Changed export failed names to display file name

--- AssetStudioCLI/Studio.cs
+++ AssetStudioCLI/Studio.cs
@@ -394,19 +394,19 @@ namespace AssetStudioCLI
                 case ExportType.Raw:
                     if (!ExportRawFile(asset, exportPath))
                     {
-                        exportFailedNames.Add(asset.SourceFile.fullName);
+                        exportFailedNames.Add(asset.SourceFile.fileName);
                     }
                     break;
                 case ExportType.Dump:
                     if (!ExportDumpFile(asset, exportPath))
                     {
-                        exportFailedNames.Add(asset.SourceFile.fullName);
+                        exportFailedNames.Add(asset.SourceFile.fileName);
                     }
                     break;
                 case ExportType.Convert:
                     if (!ExportConvertFile(asset, exportPath))
                     {
-                        exportFailedNames.Add(asset.SourceFile.fullName);
+                        exportFailedNames.Add(asset.SourceFile.fileName);
                     }
                     break;
             }
